e8558a1f7240cc29049cf8dae6f86f1b83b476e8
lvalencia37 <lvalencia@grupogarflo.com>
2022-05-06 18:12:26 -0500

    Modal de nueva venta y icon de tab del navegador

--- src/views/VentaView.vue
+++ src/views/VentaView.vue
@@ -19,6 +19,9 @@
 
          <v-row>
            <v-col cols="12" sm="6">
+             <v-btn depressed color="primary" @click="modalnewventa = !modalnewventa">
+                Nueva Venta
+              </v-btn>
            </v-col>
 
            <v-col cols="12" sm="6">
@@ -37,6 +40,65 @@
                </v-icon>
              </template>
           </v-data-table>
+
+          <!--- Model Nueva venta -->
+          <v-dialog v-model="modalnewventa" transition="dialog-top-transition" max-width="900">
+            <template v-slot:default="dialog">
+              <v-card>
+                <v-toolbar color="primary" dark>Nueva venta</v-toolbar>
+                <v-card-text>
+
+                  <v-row>
+                    <v-col cols="12" sm="8">
+                      <v-card-text>Crear Venta</v-card-text>
+                      <v-form ref="formvalidventanew" v-model="validformventanew" lazy-validation>
+
+                        <v-select v-model="selectclient" :items="itemclients" item-text="text" item-value="value" :rules="[() => !!selectclient || 'Seleccione una cliente']" label="Seleccione una cliente" outlined required>
+                        </v-select>
+
+                        <v-row>
+                          <v-col cols="12" sm="10">
+                            <v-select v-model="selectproduct" :items="itemsproducts" item-text="text" item-value="value" :rules="[() => !!selectproduct || 'Seleccione un productos']" label="Seleccione un productos" outlined required>
+                            </v-select>
+                          </v-col>
+                          <v-col cols="12" sm="2">
+                            <v-btn class="mx-2" fab dark color="indigo" @click="btnaddproduct">
+                              <v-icon dark>
+                                mdi-plus
+                              </v-icon>
+                            </v-btn>
+                          </v-col>
+                        </v-row>
+
+                        <v-select v-model="selectpagotype" :items="itemspagostype" item-text="text" item-value="value" :rules="[() => !!selectpagotype || 'Seleccione el tipo de pago']" label="Seleccione el tipo de pago" outlined required>
+                        </v-select>
+                      </v-form>
+                    </v-col>
+                    <v-col cols="12" sm="4">
+                      <h3>Nueva venta</h3>
+                      <table>
+                        <thead>
+                          <tr bgcolor="#6043ff">
+                            <th>Cantidad</th>
+                            <th>Producto</th>
+                            <th>Precio</th>
+                            <th>Accion</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                      </table>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+                <v-card-actions class="justify-end">
+                  <v-btn large color="secondary" @click="dialog.value = false">Cancelar</v-btn>
+                  <v-btn large color="primary" @click="paypalventa">Pagar</v-btn>
+                </v-card-actions>
+              </v-card>
+            </template>
+          </v-dialog>
+
 
           <!--- Model editar -->
           <v-dialog v-model="modaldetailventa" transition="dialog-top-transition" max-width="600">
@@ -125,7 +187,15 @@ export default {
         clienteventa:'',
         userventa:'',
         pagoventa:'',
-        datosdetailtable: []
+        datosdetailtable: [],
+        modalnewventa:false,
+        validformventanew:true,
+        selectclient:'',
+        itemclients:[],
+        selectproduct:'',
+        itemsproducts:[],
+        selectpagotype:'',
+        itemspagostype:[]
       }
   },
   methods:{
@@ -152,6 +222,67 @@ export default {
         console.log(error);
       });
 
+    },
+    loadingclients(){
+      var formdataclients = new FormData();
+      formdataclients.append('api_token',localStorage.getItem('token_user'));
+
+      this.axios.post(process.env.VUE_APP_URL + '/cliente/get/all',formdataclients).then((response) => {
+        if(response.data.code == 200){
+
+          var datosclients = response.data.data;
+          this.itemclients = [];
+          datosclients.forEach((client) => {
+            this.itemclients.push({"value":client['id'],"text":client['nombre']});
+          });
+          this.overlay = false;
+
+        }
+      }).catch((error) =>{
+        this.overlay = false;
+        console.log("Error en el try catch");
+        console.log(error);
+      });
+    },
+    loadingproducts(){
+      var formdataproducts = new FormData();
+      formdataproducts.append('api_token',localStorage.getItem('token_user'));
+
+      this.axios.post(process.env.VUE_APP_URL + '/producto/get/all',formdataproducts).then((response) => {
+        if(response.data.code == 200){
+
+          var datosproducts = response.data.data;
+          this.itemsproducts = [];
+          datosproducts.forEach((product) => {
+            this.itemsproducts.push({"value":product['id'],"text":product['nombre']});
+          });
+          this.overlay = false;
+
+        }
+      }).catch((error) =>{
+        this.overlay = false;
+        console.log("Error en el try catch");
+        console.log(error);
+      });
+    },
+    loadingtypepago(){
+
+      this.axios.get(process.env.VUE_APP_URL + '/get/pago').then((response) => {
+        if(response.data.code == 200){
+
+          var datostypepago = response.data.data;
+          this.itemspagostype = [];
+          datostypepago.forEach((typepay) => {
+            this.itemspagostype.push({"value":typepay['id'],"text":typepay['name']});
+          });
+          this.overlay = false;
+
+        }
+      }).catch((error) =>{
+        this.overlay = false;
+        console.log("Error en el try catch");
+        console.log(error);
+      });
     },
     editar(data){
       this.overlay =  true;
@@ -190,12 +321,20 @@ export default {
         console.log(error);
       });
       this.overlay = false;
-
+    },
+    btnaddproduct(){
+      console.log("----------Agregando producto-------------");
+      console.log(this.selectproduct);
+    },
+    paypalventa(){
+      console.log("-------------recontandando datos de la venta---------------");
     }
-
   },
   mounted(){
     this.loadingtabledata();
+    this.loadingclients();
+    this.loadingproducts();
+    this.loadingtypepago();
   }
 }
 </script>
